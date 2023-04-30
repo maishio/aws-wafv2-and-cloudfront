3cfd33b6bcec6b852c7fbd7713365e4242bafefb
maishio <masanori.ishio119@gmail.com>
2023-04-30 16:43:53 +0900

    feat: add alb_id output variable

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,3 +1,8 @@
+output "alb_id" {
+  description = "ID of the ALB."
+  value       = module.alb.elb.id
+}
+
 output "alb_target_group_arn" {
   description = "ARN of the ALB target group."
   value       = module.target_group.elb_target_group.arn
