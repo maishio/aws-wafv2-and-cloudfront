1929c33eea2f11ab0acc344718fe0f47692cfe3f
maishio <masanori.ishio119@gmail.com>
2023-04-30 10:29:06 +0900

    refactor: remove amazon s3 bucket acl resource

--- terraform/resources/s3/bucket_acl/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Amazon S3 Bucket ACL
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
-
-output "s3_bucket_acl" {
-  description = "Amazon S3 Bucket ACL resource."
-  value       = aws_s3_bucket_acl.this
-}
