3cc7dc06d84d1028591db1eed43ab61a21135fa5
maishio <masanori.ishio119@gmail.com>
2023-05-01 20:40:02 +0900

    refactor: refactor: moved to vpc directory

--- terraform/modules/security_group/alb.tf
+++ terraform/modules/security_group/alb.tf
@@ -1,15 +1,16 @@
 module "alb_sg" {
-  source              = "../../resources/security_group"
+  source              = "../../resources/vpc/security_group"
   security_group_name = "${var.tags.service}-${var.tags.env}-alb-sg"
   tags                = var.tags
   vpc_id              = var.vpc_id
   ingress_rule = {
     0 = {
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "Internet"
+      description = "Allow CloudFront"
       from_port   = 443
-      protocol    = "tcp"
-      to_port     = 443
+      /* Prefix list to allow access via CloudFront */
+      prefix_list_ids = [data.aws_ec2_managed_prefix_list.this.id]
+      protocol        = "tcp"
+      to_port         = 443
     }
   }
 }
--- terraform/modules/security_group/ecs.tf
+++ terraform/modules/security_group/ecs.tf
@@ -1,11 +1,11 @@
 module "ecs_sg" {
-  source              = "../../resources/security_group"
+  source              = "../../resources/vpc/security_group"
   security_group_name = "${var.tags.service}-${var.tags.env}-ecs-sg"
   tags                = var.tags
   vpc_id              = var.vpc_id
   ingress_rule = {
     0 = {
-      description              = "ALB"
+      description              = "Allow ALB"
       from_port                = 8000
       protocol                 = "tcp"
       source_security_group_id = module.alb_sg.security_group.id
