06dba016c9953efc1022a6f4132b6956dcd5bf99
maishio <masanori.ishio119@gmail.com>
2023-05-01 17:24:01 +0900

    refactor: remove output variable alb_logs_bucket_id

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -7,8 +7,3 @@ output "alb_target_group_arn" {
   description = "ARN of the ALB target group."
   value       = module.target_group.elb_target_group.arn
 }
-
-output "alb_logs_bucket_id" {
-  description = "ID of the ALB log bucket."
-  value       = module.alb_logs.s3_bucket.id
-}
