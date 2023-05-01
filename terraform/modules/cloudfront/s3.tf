eb81176fe641cefe933787a4789ae1c4b2c4d0c7
maishio <masanori.ishio119@gmail.com>
2023-05-01 17:29:13 +0900

    refactor: rename s3 module to contents

--- terraform/modules/cloudfront/s3.tf
+++ terraform/modules/cloudfront/s3.tf
@@ -1,31 +1,31 @@
-module "s3" {
+module "contents" {
   source        = "../../resources/s3/bucket"
-  bucket        = "${var.tags.service}-${var.tags.env}-cloudfront-bucket"
+  bucket        = "${var.tags.service}-${var.tags.env}-contents"
   force_destroy = true
   tags          = var.tags
 }
 
-module "s3_policy" {
+module "contents_policy" {
   source = "../../resources/s3/bucket_policy"
-  bucket = module.s3.s3_bucket.id
+  bucket = module.contents.s3_bucket.id
   path   = "${path.module}/files/template/cloudfront_s3_bucket_policy.json.tpl"
   vars = {
-    BUCKET     = module.s3.s3_bucket.id
+    BUCKET     = module.contents.s3_bucket.id
     CLOUDFRONT = module.origin_access_identity.cloudfront_origin_access_identity.iam_arn
   }
 }
 
-module "s3_versioning" {
+module "contents_versioning" {
   source = "../../resources/s3/bucket_versioning"
-  bucket = module.s3.s3_bucket.id
+  bucket = module.contents.s3_bucket.id
   status = "Enabled"
 }
 
-module "s3_object" {
+module "contents_object" {
   source       = "../../resources/s3/object"
-  bucket       = module.s3.s3_bucket.id
+  bucket       = module.contents.s3_bucket.id
   content_type = "text/html"
-  key          = "index.html"
+  key          = "static/index.html"
   path         = "${path.module}/files/object/index.html"
   tags         = var.tags
 }
