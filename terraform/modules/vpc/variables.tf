c0a84d6bb850d1995f749d67fd3096f27abbc31f
maishio <masanori.ishio119@gmail.com>
2023-05-01 17:32:29 +0900

    refactor: add account variable

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "account" {
+  description = "AWS Account ID."
+  type        = map(string)
+}
+
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
