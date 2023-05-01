143e61bbf3b19dff6496b69fd4ac0c848d1e7ecd
maishio <masanori.ishio119@gmail.com>
2023-05-01 17:27:20 +0900

    refactor: add aws_cloudfront_cache_policy data source

--- terraform/modules/cloudfront/data.tf
+++ terraform/modules/cloudfront/data.tf
@@ -5,3 +5,41 @@ data "aws_route53_zone" "this" {
   name         = var.hostzone.name
   private_zone = false
 }
+
+/* List of available managed cache policy names.
+aws cloudfront list-cache-policies --query 'CachePolicyList.Items[].CachePolicy.CachePolicyConfig.Name'
+[
+    "Managed-CachingOptimized",
+    "Managed-CachingDisabled",
+    "Managed-CachingOptimizedForUncompressedObjects",
+    "Managed-Elemental-MediaPackage",
+    "Managed-Amplify"
+]
+*/
+
+# Managed Cache Policies
+# https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
+
+data "aws_cloudfront_cache_policy" "this" {
+  name = "Managed-Elemental-MediaPackage"
+}
+
+/* List of available managed origin request policy names.
+aws cloudfront list-origin-request-policies --query 'OriginRequestPolicyList.Items[].OriginRequestPolicy.OriginRequestPolicyConfig.Name'
+[
+    "Managed-UserAgentRefererHeaders",
+    "Managed-AllViewer",
+    "Managed-CORS-S3Origin",
+    "Managed-CORS-CustomOrigin",
+    "Managed-Elemental-MediaTailor-PersonalizedManifests",
+    "Managed-AllViewerAndCloudFrontHeaders-2022-06",
+    "Managed-AllViewerExceptHostHeader"
+]
+*/
+
+# Managed Origin Request Policies
+# https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/using-managed-origin-request-policies.html
+
+data "aws_cloudfront_origin_request_policy" "asset" {
+  name = "Managed-CORS-S3Origin"
+}
