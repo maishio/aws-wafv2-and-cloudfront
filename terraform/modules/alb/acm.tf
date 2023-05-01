cecf44bc94c45fda8e0aab6091e77c16bcc334cc
maishio <masanori.ishio119@gmail.com>
2023-05-01 17:20:06 +0900

    refactor: change the value of domain_name

--- terraform/modules/alb/acm.tf
+++ terraform/modules/alb/acm.tf
@@ -1,7 +1,7 @@
 module "acm" {
   source                    = "../../resources/acm"
-  domain_name               = "*.${data.aws_route53_zone.this.name}"
-  subject_alternative_names = [data.aws_route53_zone.this.name]
+  domain_name               = "alb.${data.aws_route53_zone.this.name}"
+  subject_alternative_names = ["*.alb.${data.aws_route53_zone.this.name}"]
   tags                      = var.tags
   validation_method         = "DNS"
   zone_id                   = data.aws_route53_zone.this.zone_id
