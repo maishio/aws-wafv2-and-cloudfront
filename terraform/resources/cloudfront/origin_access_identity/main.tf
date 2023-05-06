75cc62dcbf9fb24d0858bcd0eabd233476592095
maishio <masanori.ishio119@gmail.com>
2023-05-06 22:10:14 +0900

    refactor: remove cloudfront origin access identity resource

--- terraform/resources/cloudfront/origin_access_identity/main.tf
+++
@@ -1,6 +0,0 @@
-# Amazon CloudFront Origin Access Identity
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_identity
-
-resource "aws_cloudfront_origin_access_identity" "this" {
-  comment = var.comment
-}
