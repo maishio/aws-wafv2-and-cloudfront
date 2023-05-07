64601828cc8d80b8efaafe1a478b0bf8d215af7f
maishio <masanori.ishio119@gmail.com>
2023-05-07 15:43:18 +0900

    refactor: remove acm for ALB

--- terraform/modules/alb/acm.tf
+++
@@ -1,8 +0,0 @@
-module "acm" {
-  source                    = "../../resources/acm"
-  domain_name               = "alb.${data.aws_route53_zone.this.name}"
-  subject_alternative_names = ["*.alb.${data.aws_route53_zone.this.name}"]
-  tags                      = var.tags
-  validation_method         = "DNS"
-  zone_id                   = data.aws_route53_zone.this.zone_id
-}
